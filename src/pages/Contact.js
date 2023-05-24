9aa36758bd8d2429d47f2ae6459e28efd51cd252
pbienek <pawelbienek@gmail.com>
2023-05-24 09:37:03 +0200

    Update CSSmodule and ReactRouterDom version, improve ContactPage styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -63,27 +63,29 @@ const ContactPage = () => {
         <section className={classes.contact}>
           <div className={classes.card}>
             <h1>contact us</h1>
-            <section className={classes.open_hours}>
-              <h2>Hours open</h2>
-              <ul>
-                <li>Monday 8:00 - 16:00</li>
-                <li>Tuesday 8:00 - 16:00</li>
-                <li>Wednesday 8:00 - 16:00</li>
-                <li>Thursday 8:00 - 16:00</li>
-                <li>Friday 8:00 - 16:00</li>
-                <li>Saturday 9:00 - 13:00</li>
-                <li>Sunday closed</li>
-              </ul>
-            </section>
-            <section className={classes.data}>
-              <h2>Company data</h2>
-              <span>SKIS POLAND S.A.</span>
-              <span>ul. Magnolii</span>
-              <span>44 - 200 Racibórz</span>
-              <span>info@skrzynka.skispoland.pl</span>
-              <h3>ING Bank Śląski</h3>
-              <span>Account number: 10 1010 1010 1010 1010 1010 1010</span>
-            </section>
+            <div className={classes.contactData}>
+              <section className={classes.open_hours}>
+                <h2>Hours open</h2>
+                <ul>
+                  <li>Monday 8:00 - 16:00</li>
+                  <li>Tuesday 8:00 - 16:00</li>
+                  <li>Wednesday 8:00 - 16:00</li>
+                  <li>Thursday 8:00 - 16:00</li>
+                  <li>Friday 8:00 - 16:00</li>
+                  <li>Saturday 9:00 - 13:00</li>
+                  <li>Sunday closed</li>
+                </ul>
+              </section>
+              <section className={classes.data}>
+                <h2>Company data</h2>
+                <span>SKIS POLAND S.A.</span>
+                <span>ul. Magnolii</span>
+                <span>44 - 200 Racibórz</span>
+                <span>info@skrzynka.skispoland.pl</span>
+                <h3>ING Bank Śląski</h3>
+                <span>Account number: 10 1010 1010 1010 1010 1010 1010</span>
+              </section>
+            </div>
           </div>
         </section>
 
